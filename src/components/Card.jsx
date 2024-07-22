70fe4a34dc5e297a7619f8f82d5e0e6b7a3b42c0
Adarsh Gharti <adarsh.gcdev@gmail.com>
2024-07-22 20:58:03 +0545

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({ children, bg = 'bg-gray-100' }) => {
+    return (
+        // <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
+        <div className={`${bg} p-6 rounded-lg shadow-md`} >
+
+            {children}
+
+        </div >
+    )
+}
+
+export default Card
